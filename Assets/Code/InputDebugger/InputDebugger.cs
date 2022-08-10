835f07388daec8cb67130d9517e5c7f2fc76611c
Nedislav Stoychev <nedislav.stoichev@gmail.com>
2022-08-10 22:04:52 +0300

    Adding label input display and some test icons

--- Assets/Code/InputDebugger/InputDebugger.cs
+++ Assets/Code/InputDebugger/InputDebugger.cs
@@ -1,6 +1,5 @@
 using Code.InputActions;
 using Code.Styles;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,8 +8,16 @@ namespace Code.InputDebugger
 {
 	public class InputDebugger : MonoBehaviour
 	{
+		public bool Enabled = false;
+
 		[Header("Capture settings")]
 		public InputDebugVisualStyle InputActions;
+		public Vector2 InputIconsSize = new Vector2(64, 64);
+		public GUIStyle DebugTextStyle;
+		public string AxisValueLabelPattern = "{0} {1}";
+
+		public bool CaptureMousePosition = false;
+		public string MousePositionLabelPattern = "MousePos: {0}x {1}y";
 
 		[Header("Position")]
 		public DebugStyleAnchorType DrawAnchor = DebugStyleAnchorType.TopLeft;
@@ -18,8 +25,6 @@ namespace Code.InputDebugger
 		public int DrawYOffset = 0;
 
 		private DebugStyleAnchorType _drawAnchorInternal;
-
-		public int labelCount = 5;
 
 		private List<KeyInputAction> _captureKeys = new List<KeyInputAction>();
 		private List<AxisInputAction> _captureAxes = new List<AxisInputAction>();
@@ -32,17 +37,12 @@ namespace Code.InputDebugger
 			UpdateCapturedInputActions();
 		}
 
-		public void UpdateCapturedInputActions()
-		{
-			_captureKeys.Clear();
-			_captureKeys.AddRange(InputActions.Keys);
-
-			_captureAxes.Clear();
-			_captureAxes.AddRange(InputActions.Axes);
-		}
-
 		private void Update()
 		{
+			if (!Enabled) {
+				return;
+			}
+
 			_displayKeyActions.Clear();
 			_displayAxisActions.Clear();
 
@@ -68,7 +68,7 @@ namespace Code.InputDebugger
 				SetAnchor(DrawAnchor);
 			}
 
-			//Begin layout. Vertical with inner Horizontal - content in the middle
+			//Begin layout. Vertical with inner Horizontals - content in the middle
 			GUILayout.BeginArea(new Rect(DrawXOffset, DrawYOffset, Screen.width - DrawXOffset, Screen.height - DrawYOffset));
 			GUILayout.BeginVertical();
 
@@ -119,7 +119,7 @@ namespace Code.InputDebugger
 				GUILayout.FlexibleSpace();
 			}
 
-			DrawINputAxisActions();
+			DrawInputAxisActions();
 
 			if (_drawAnchorInternal == DebugStyleAnchorType.BottomLeft
 			|| _drawAnchorInternal == DebugStyleAnchorType.TopLeft
@@ -131,6 +131,34 @@ namespace Code.InputDebugger
 			}
 
 			GUILayout.EndHorizontal();
+			#endregion
+
+			#region Draw Capture Mouse Position
+			if (CaptureMousePosition) {
+				GUILayout.BeginHorizontal();
+
+				if (_drawAnchorInternal == DebugStyleAnchorType.BottomRight
+				|| _drawAnchorInternal == DebugStyleAnchorType.TopRight
+				|| _drawAnchorInternal == DebugStyleAnchorType.MiddleRight
+				|| _drawAnchorInternal == DebugStyleAnchorType.Middle
+				|| _drawAnchorInternal == DebugStyleAnchorType.TopCenter
+				|| _drawAnchorInternal == DebugStyleAnchorType.BottomCenter) {
+					GUILayout.FlexibleSpace();
+				}
+
+				DrawMousePosition();
+
+				if (_drawAnchorInternal == DebugStyleAnchorType.BottomLeft
+				|| _drawAnchorInternal == DebugStyleAnchorType.TopLeft
+				|| _drawAnchorInternal == DebugStyleAnchorType.MiddleLeft
+				|| _drawAnchorInternal == DebugStyleAnchorType.Middle
+				|| _drawAnchorInternal == DebugStyleAnchorType.TopCenter
+				|| _drawAnchorInternal == DebugStyleAnchorType.BottomCenter) {
+					GUILayout.FlexibleSpace();
+				}
+
+				GUILayout.EndHorizontal();
+			}
 			#endregion
 
 			if (_drawAnchorInternal == DebugStyleAnchorType.Middle
@@ -146,8 +174,21 @@ namespace Code.InputDebugger
 			GUILayout.EndArea();
 		}
 
+		public void UpdateCapturedInputActions()
+		{
+			_captureKeys.Clear();
+			_captureKeys.AddRange(InputActions.Keys);
+
+			_captureAxes.Clear();
+			_captureAxes.AddRange(InputActions.Axes);
+		}
+
 		private void SetAnchor(DebugStyleAnchorType newAnchor)
 		{
+			if (newAnchor == DebugStyleAnchorType.None) {
+				newAnchor = DebugStyleAnchorType.TopLeft;
+			}
+
 			_drawAnchorInternal = DrawAnchor;
 		}
 
@@ -155,18 +196,56 @@ namespace Code.InputDebugger
 		{
 			if (_displayKeyActions.Any()) {
 				foreach (KeyInputAction keyAction in _displayKeyActions) {
-					GUILayout.Label(keyAction.Name.ToString());
+					if (keyAction.Icon == null) {
+						GUILayout.Label(keyAction.Name.ToString(), DebugTextStyle);
+					}
+					else {
+						GUIStyle protoStyle = new GUIStyle();
+						protoStyle.normal.background = keyAction.Icon.texture;
+						GUILayout.Box(Texture2D.blackTexture, protoStyle, GUILayout.Width(InputIconsSize.x), GUILayout.Height(InputIconsSize.y));
+					}
 				}
 			}
 		}
 
-		private void DrawINputAxisActions()
+		private void DrawInputAxisActions()
 		{
 			if (_displayAxisActions.Any()) {
 				foreach (KeyValuePair<AxisInputAction, float> axisInputActionPair in _displayAxisActions) {
-					GUILayout.Label(axisInputActionPair.Key.AxisName.ToString());
+					if (axisInputActionPair.Value > 0) {
+						if (axisInputActionPair.Key.PositiveValueIcon == null) {
+							DrawAxisLabel(axisInputActionPair);
+						}
+						else {
+							GUILayout.Box(axisInputActionPair.Key.PositiveValueIcon.texture);
+						}
+					}
+					else if (axisInputActionPair.Value < 0) {
+						if (axisInputActionPair.Key.NegativeValueIcon== null) {
+							DrawAxisLabel(axisInputActionPair);
+						}
+						else {
+							GUILayout.Box(axisInputActionPair.Key.NegativeValueIcon.texture);
+						}
+					}
 				}
 			}
+		}
+
+		private void DrawAxisLabel(KeyValuePair<AxisInputAction, float> axisPair)
+		{
+			GUILayout.Label(
+				string.Format(AxisValueLabelPattern,
+					axisPair.Key.AxisName.ToString(),
+					axisPair.Value.ToString()
+				), 
+				DebugTextStyle
+			);
+		}
+
+		private void DrawMousePosition()
+		{
+			GUILayout.Label(string.Format(MousePositionLabelPattern, Input.mousePosition.x, Input.mousePosition.y), DebugTextStyle);
 		}
 
 		[ContextMenu("Update Input Actions")]
